4413afe5b13fdb73371e4a2cbfbe1efe7e00428d
Lilia10010 <liliaxtz2@gmail.com>
2025-08-13 20:20:18 -0300

    norm execute builtin

--- src/executor/execute_builtin_with_redirection.c
+++ src/executor/execute_builtin_with_redirection.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_builtin_with_redirection.c                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpaula-n <lpaula-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: microbiana <microbiana@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 21:28:49 by lpaula-n          #+#    #+#             */
-/*   Updated: 2025/08/10 23:29:25 by lpaula-n         ###   ########.fr       */
+/*   Updated: 2025/08/13 18:59:19 by microbiana       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
