bad478c58a2dfa1b49ecb011dfb6d88fc46e2021
Lilia10010 <liliaxtz2@gmail.com>
2025-08-14 13:12:40 -0300

    fix norm

--- src/executor/find_executable_in_path.c
+++ src/executor/find_executable_in_path.c
@@ -6,7 +6,7 @@
 /*   By: microbiana <microbiana@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 21:47:45 by lpaula-n          #+#    #+#             */
-/*   Updated: 2025/08/05 13:53:08 by microbiana       ###   ########.fr       */
+/*   Updated: 2025/08/14 13:09:27 by microbiana       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "executor.h"
 #include "lib_ft.h"
 
-static char *join_path(const char *dir, const char *cmd)
+static char	*join_path(const char *dir, const char *cmd)
 {
 	char	*temp;
 	char	*full;
@@ -29,7 +29,7 @@ static char *join_path(const char *dir, const char *cmd)
 	return (full);
 }
 
-char *find_executable_in_path(const char *cmd)
+char	*find_executable_in_path(const char *cmd)
 {
 	char	*path_env;
 	char	**paths;
@@ -38,8 +38,6 @@ char *find_executable_in_path(const char *cmd)
 
 	if (!cmd || !*cmd)
 		return (NULL);
-	/* if (ft_strchr(cmd, '/'))
-		return (NULL); */
 	path_env = getenv("PATH");
 	if (!path_env)
 		return (NULL);
