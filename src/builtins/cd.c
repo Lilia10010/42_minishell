b31c7dbe54c99b3e2ef6967696ed4e09a6a961dd
Melissa <memejaragua@gmail.com>
2025-08-18 22:10:25 -0300

    builtins corrigidas

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 18:58:24 by meandrad          #+#    #+#             */
-/*   Updated: 2025/08/18 08:44:39 by meandrad         ###   ########.fr       */
+/*   Updated: 2025/08/18 21:47:45 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ static void	cd_error(char *path)
 	ft_putstr_fd(": no such file or directory\n", 2);
 }
 
-static char	*get_target_path(char **args, char **envp)
+static char	*get_target_path(char **args, t_context *ctx)
 {
 	char	*home_path;
 
 	if (!args[1])
 	{
-		home_path = get_env_value("HOME", envp);
+		home_path = get_env_value("HOME", ctx);
 		if (!home_path)
 		{
 			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
@@ -40,19 +40,19 @@ static char	*get_target_path(char **args, char **envp)
 	return (ft_strdup(args[1]));
 }
 
-static int	update_path_var(char **envp, char *old_path)
+static int	update_path_var(t_context *ctx, char *old_path)
 {
 	char	*new_path;
 
 	new_path = getcwd(NULL, 0);
 	if (!new_path)
 		return (1);
-	if (set_env(envp, "OLDPWD", old_path) != 0)
+	if (set_env(&ctx->envp, "OLDPWD", old_path) != 0)
 	{
 		free(new_path);
 		return (1);
 	}
-	if (set_env(envp, "PWD", new_path) != 0)
+	if (set_env(&ctx->envp, "PWD", new_path) != 0)
 	{
 		free(new_path);
 		return (1);
@@ -61,7 +61,7 @@ static int	update_path_var(char **envp, char *old_path)
 	return (0);
 }
 
-static int	change_directory(char *path, char **envp)
+static int	change_directory(char *path, t_context *ctx)
 {
 	char	*old_path;
 	int		result;
@@ -75,7 +75,7 @@ static int	change_directory(char *path, char **envp)
 		free(old_path);
 		return (1);
 	}
-	result = update_path_var(envp, old_path);
+	result = update_path_var(ctx, old_path);
 	free(old_path);
 	return (result);
 }
@@ -85,13 +85,13 @@ int	builtin_cd(char **args, t_context *ctx)
 	char	*target_path;
 	int		result;
 
-	target_path = get_target_path(args, ctx->envp);
+	target_path = get_target_path(args, ctx);
 	if (!target_path)
 	{
 		ctx->exit_status = 1;
 		return (1);
 	}
-	result = change_directory(target_path, ctx->envp);
+	result = change_directory(target_path, ctx);
 	free(target_path);
 	ctx->exit_status = result;
 	return (result);
